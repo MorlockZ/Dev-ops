4e7d8b3f3237fe20a17d1ef109e7528a6e9f97f7
2364173153@qq.com <2364173153@qq.com>
2018-06-07 14:05:54 +0800

    tj
    123

---
+++ Task-郑雯文试用期第一周/Task-郑雯文试用期第四周/郑雯文.py
@@ -0,0 +1,12 @@
+#from bs4 import BeautifulSoup
+import requests
+from pyquery import PyQuery as pq  #用来解析HTML文件的包 ，别名是pq
+newsurl = 'https://m.hupu.com/bbs/20415703.html'   #需要解析的网址
+res = requests.get(newsurl)   #获取网址返回的htmlb编码
+print (res.text)
+d = pq(res.text)     #把返回的编码放入pq并赋值给d
+print (d.html())
+nr = d('span').filter('.short-content').text()  #选择span标签并筛选.short-content文本（text）
+f = open(r'e:\test.txt','w+')
+f.write(nr)
+f.close() 
